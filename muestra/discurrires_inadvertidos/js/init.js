065968cbb15e59a2a2c8f7372b2ee91428e9669d
hebe-argentieri <90655472+hebe-argentieri@users.noreply.github.com>
2021-10-25 16:26:17 -0300

    redistribucion de funciones

--- muestra/discurrires_inadvertidos/js/init.js
+++ muestra/discurrires_inadvertidos/js/init.js
@@ -11,27 +11,6 @@ _.templateSettings = {
   escape: /\[\[--([\s\S]+?)\]--\]/g
 };
 
-const ampliar_obra = function (id){
-    let cnt_obra = _.template($('#'+id).html());
-    const mostrar = function(e){
-	let cf = 'activo obra',
-	    cb = 'bloqueado';
-	if(e){
-	    G.panel_flotante.addClass(cf);
-	    G.body.addClass(cb);
-	}else{
-	    G.panel_flotante.removeClass(cf);
-	    G.body.removeClass(cb)
-	}
-    }
-    G.panel_flotante.find('.contenido').eq(0).html(cnt_obra);
-    mostrar(true);
-    G.panel_flotante.find('.cerrar').on({
-	click: function(){
-	    mostrar(false);
-	}
-    });
-}
 
 /* initialization */
 $( window ).on({
